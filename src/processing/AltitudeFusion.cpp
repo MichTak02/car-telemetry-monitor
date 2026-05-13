8f949f512714d0110c7b552b8da233a5142d2fd8
Michal Takac <xtakac@fi.muni.cz>
2026-05-13 17:57:09 +0200

    feat(altitude): convert barometric pressure to altitude

--- src/processing/AltitudeFusion.cpp
+++ src/processing/AltitudeFusion.cpp
@@ -1,5 +1,12 @@
 #include "AltitudeFusion.h"
 
+
+float AltitudeFusion::pressureToAltitude(float pressureHpa)
+{
+    // Barometric formula to convert pressure to altitude (https://ncar.github.io/aircraft_ProcessingAlgorithms/3-the-state-of-the-aircraft.html#palt)
+    return 44330.77 * (1.0 - powf(pressureHpa / SEA_LEVEL_PRESSURE, 0.1902632f));
+}
+
 void AltitudeFusion::enable()
 {
     _altitude = 0.0f;
@@ -16,19 +23,21 @@ void AltitudeFusion::update()
     if (!_enabled) {
         return;
     }
-    FloatSample barometerAltitude = _barometer.getSample();
+
+    FloatSample barometerPressure = _barometer.getSample(UNIT_HPA);
+    float barometerAltitude = pressureToAltitude(barometerPressure.value);
 
     if (!_gps.isValid()) {
         // If GPS data is not valid, use only barometer
-        _altitude = barometerAltitude.value;
+        _altitude = barometerAltitude;
         return;
     }
 
     GPSSample gpsSample = _gps.getSample();
-
+    
     // Use complementary filter to fuse GPS and barometer altitude
     float gpsAltitude = gpsSample.altitude;
-    _altitude = _alpha * gpsAltitude + (1 - _alpha) * barometerAltitude.value;
+    _altitude = _alpha * gpsAltitude + (1 - _alpha) * barometerAltitude;
 }
 
 float AltitudeFusion::getAltitude()
--- src/processing/AltitudeFusion.h
+++ src/processing/AltitudeFusion.h
@@ -8,11 +8,15 @@
 
 class AltitudeFusion {
     private:
+        static constexpr float SEA_LEVEL_PRESSURE = 1013.25f; // in hPa
+
         bool _enabled = false;
         const float _alpha = 0.98;
         float _altitude;
         GPS& _gps;
         Barometer& _barometer;
+        
+        float pressureToAltitude(float pressure);
 
     public:
         AltitudeFusion(GPS& gps, Barometer& barometer) : _altitude(0.0f), _gps(gps), _barometer(barometer) {}
